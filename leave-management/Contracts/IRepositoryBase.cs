5fa585ca56e416fcb08b5759a92366eb77ffd815
Jasmine Logan <jasmine.logan@pharmacyfirst.com>
2020-04-14 00:05:50 -0500

    Add project files.

---
+++ leave-management/Contracts/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Contracts
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        ICollection<T> FindAll();
+        T FindByID(int id);
+        T Create(T entity);
+        T Update(T entity);
+        T Delete(T entity);
+        bool Save();
+    }
+}
---
+++ leave-management/Repository/LeaveTypeRepository.cs
@@ -0,0 +1,42 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveTypeRepository : ILeaveTypeRepository
+    {
+        public LeaveType Create(LeaveType entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveType Delete(LeaveType entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveType> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveType FindByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveType Update(LeaveType entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
